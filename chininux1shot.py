9a6a568faed08efd74c17d4e5f053ec30ead9c4b
Claudio Pisa <clauz@ninux.org>
2014-09-26 22:19:46 +0200

    move most of the logic to the processtable() function

--- chininux1shot.py
+++ chininux1shot.py
@@ -11,7 +11,8 @@ GIURL = "http://wiki.ninux.org/GestioneIndirizzi"
 query = sys.argv[1]
 
 class row():
-    def __init__(self, labels):
+    def __init__(self, section, labels):
+        self.section = section # the name of the page section
         self.labels = labels # keep 'em sorted
     def search(self, q):
         "search q in all field values and return a similarity ratio"
@@ -29,27 +30,23 @@ class row():
                 r += 1.0
         return r
     def __repr__(self):
-        r = ""
+        r = self.section
         for label in self.labels:
             r += "%s: %s\n" % (label, getattr(self, label))
         return r
     def __str__(self):
         return repr(self)
 
-html_doc = urllib2.urlopen(GIURL)
-
-soup = BeautifulSoup(html_doc)
-
-rows = []
-tables = soup.find_all('table')
-for table in tables:
+def processtable(table, currentsection=""):
+    "return a list of row objects from a table"
+    rrows = []
     labels = []
     for tr in table.find_all('tr'):
         tds = tr.find_all('td')
         if len(labels) == 0:
             labels = [next(td.strings).replace(' ', '_').replace('.', '_') for td in tds]
         else:
-            r = row(labels)
+            r = row(currentsection, labels)
             for i in range(len(tds)):
                 try:
                     label = labels[i]
@@ -57,8 +54,16 @@ for table in tables:
                     continue
                 data = tds[i].get_text()
                 setattr(r, label, data)
-            rows.append(r)
+            rrows.append(r)
+    return rrows
 
+html_doc = urllib2.urlopen(GIURL)
+soup = BeautifulSoup(html_doc)
+
+rows = []
+currentsection = ""
+for t in soup.find_all("table"):
+    rows.extend(processtable(t, currentsection))
 
 for row in rows:
     if row.search(query) > 0.0:
