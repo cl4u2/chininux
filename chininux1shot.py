32fdb971f7ed13c3677b3ea00a25c1df87ffd78a
Claudio Pisa <clauz@ninux.org>
2014-09-26 23:22:48 +0200

    make chininux1shot a module

--- chininux1shot.py
+++ chininux1shot.py
@@ -7,12 +7,6 @@ from ipaddress import *
 
 GIURL = "http://wiki.ninux.org/GestioneIndirizzi"
 
-try:
-    query = sys.argv[1]
-except:
-    print "Usage: %s <IP address>" % sys.argv[0]
-    sys.exit(1)
-
 class row():
     def __init__(self, section, labels):
         self.section = section # the name of the page section
@@ -49,36 +43,58 @@ class row():
     def __str__(self):
         return repr(self)
 
-def processtable(table, currentsection=""):
-    "return a list of row objects from a table"
-    rrows = []
-    labels = []
-    for tr in table.find_all('tr'):
-        tds = tr.find_all('td')
-        if len(labels) == 0:
-            labels = [next(td.strings).replace(' ', '_').replace('.', '_') for td in tds]
-        else:
-            r = row(currentsection, labels)
-            for i in range(len(tds)):
-                try:
-                    label = labels[i]
-                except IndexError:
-                    continue
-                data = tds[i].get_text()
-                setattr(r, label, data)
-            rrows.append(r)
-    return rrows
+class AddressDirectory():
+    def __init__(self, urls):
+        "urls: list of URLs to fetch addresses from"
+        self.urls = urls
+        self.rows = []
+    def __processtable(self, table, currentsection=""):
+        "return a list of row objects from a table"
+        rrows = []
+        labels = []
+        for tr in table.find_all('tr'):
+            tds = tr.find_all('td')
+            if len(labels) == 0:
+                labels = [next(td.strings).replace(' ', '_').replace('.', '_') for td in tds]
+            else:
+                r = row(currentsection, labels)
+                for i in range(len(tds)):
+                    try:
+                        label = labels[i]
+                    except IndexError:
+                        continue
+                    data = tds[i].get_text()
+                    setattr(r, label, data)
+                rrows.append(r)
+        return rrows
+    def retrieveandparse(self, url):
+        "fetch an URL content and return a list of corresponding row objects"
+        html_doc = urllib2.urlopen(url)
+        soup = BeautifulSoup(html_doc)
+        currentsection = ""
+        for t in soup.find_all("table"):
+            currentsection = "\n".join([t.find_previous(tag).get_text() for tag in ["h1", "h2", "h3"]])
+            self.rows.extend(self.__processtable(t, currentsection))
+    def start(self):
+        for url in self.urls:
+            self.retrieveandparse(url)
+    def search(self, query):
+        "return all the rows that match a query"
+        res = []
+        for row in self.rows:
+            if row.search(query) > 0.0:
+                res.append(row)
+        return res
 
-html_doc = urllib2.urlopen(GIURL)
-soup = BeautifulSoup(html_doc)
+if __name__ == "__main__":
+    try:
+        query = sys.argv[1]
+    except:
+        print "Usage: %s <IP address>" % sys.argv[0]
+        sys.exit(1)
 
-rows = []
-currentsection = ""
-for t in soup.find_all("table"):
-    currentsection = "\n".join([t.find_previous(tag).get_text() for tag in ["h1", "h2", "h3"]])
-    rows.extend(processtable(t, currentsection))
-
-for row in rows:
-    if row.search(query) > 0.0:
-        print row
+    ad = AddressDirectory([GIURL])
+    ad.start()
+    for r in ad.search(query):
+        print r
 
